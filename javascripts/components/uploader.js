eb27b658e5789ee813f88c555fbc0faebbb08a60
Maksim Yermalovich <yermalovich.m@gmail.com>
2016-09-22 11:36:58 +0300

    Uploader - init

---
+++ javascripts/components/uploader.js
@@ -0,0 +1,43 @@
+var React = require('react'),
+    Dropzone = require('react-dropzone');
+
+var styles = {
+  dropzone: {
+    height: '200px',
+    border: 'dashed 2px #666',
+    borderRadius: '5px',
+    cursor: 'pointer'
+  },
+
+  labelContainer: {
+    height: '200px',
+    display: 'table',
+    margin: '0 50px'
+  },
+
+  label: {
+    display: 'table-cell',
+    verticalAlign: 'middle',
+    textAlign: 'center'
+  }
+};
+
+var Uploader = React.createClass({
+  handleDrop: function (files) {
+    console.log(files);
+  },
+
+  render: function () {
+    return (
+      <Dropzone onDrop={ this.handleDrop } style={ styles.dropzone }>
+        <div style={ styles.labelContainer }>
+          <div className="lead text-muted" style={ styles.label }>
+            Для загрузки, перетащите сюда файлы или кликните на область.
+          </div>
+        </div>
+      </Dropzone>
+    );
+  }
+});
+
+module.exports = Uploader;
---
+++ javascripts/index.js
@@ -0,0 +1,9 @@
+var React = require('react'),
+    ReactDOM = require('react-dom'),
+
+    Uploader = require('./components/uploader');
+
+ReactDOM.render(
+  <Uploader />,
+  document.getElementById('uploader-container')
+);
